cd92ce2c5a0464ce2e853e13affb1d6c06f78cde
NJohansen <niclas.w.j@hotmail.dk>
2020-05-18 23:01:23 +0200

    Cryptoprovider with query and insert

--- app/src/main/java/com/example/cryptoranker/provider/CryptoProvider.java
+++ app/src/main/java/com/example/cryptoranker/provider/CryptoProvider.java
@@ -1,15 +1,19 @@
-package com.example.cryptoranker;
+package com.example.cryptoranker.provider;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.room.Room;
 
 import com.example.cryptoranker.database.AppDatabase;
+import com.example.cryptoranker.database.Coin;
 import com.example.cryptoranker.database.CoinDao;
 
 public class CryptoProvider extends ContentProvider  {
@@ -22,24 +26,52 @@ public class CryptoProvider extends ContentProvider  {
 
 
     //Define the authority of the content provider
-    public static final String AUTHORITY = "com.example.cryptoranker.cryptoprovider,provider";
-    //Define database name
-    private static final String DBNAME = "db";
+    public static final String AUTHORITY = "com.example.cryptoranker.provider";
+    //Define URI for the coin table
+    public static final Uri URI_COIN = Uri.parse("content://" + AUTHORITY + "/" + Coin.TABLE_NAME);
+
+    private static final int CODE_COIN_DIR = 1;
+    private static final int CODE_COIN_ITEM = 2;
+
+    // URI matcher
+    private static final UriMatcher MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        MATCHER.addURI(AUTHORITY, Coin.TABLE_NAME, CODE_COIN_DIR);
+        MATCHER.addURI(AUTHORITY, Coin.TABLE_NAME + "/*", CODE_COIN_ITEM);
+    }
 
     @Override
     public boolean onCreate() {
-
-        //Create new database object
-        appDatabase = Room.databaseBuilder(getContext(), AppDatabase.class, DBNAME).build();
-
-        coinDao = appDatabase.coinDao();
-
         return true;
     }
 
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        final int code = MATCHER.match(URI_COIN);
+
+        if(code == CODE_COIN_DIR || code == CODE_COIN_ITEM){
+            final Context context = getContext();
+            if(context == null){
+                return null;
+            }
+            CoinDao coin = AppDatabase.getInstance(context).coinDao();
+
+            final Cursor cursor;
+
+            switch (code){
+                case 1:
+                    cursor = coin.getAll();
+                    return cursor;
+                case 2:
+                    cursor = coin.getById(ContentUris.parseId(uri));
+                    return cursor;
+                default:
+                    throw new IllegalArgumentException("Unknown URI: " + uri);
+            }
+
+        }
         return null;
     }
 
@@ -52,7 +84,19 @@ public class CryptoProvider extends ContentProvider  {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        return null;
+        switch (MATCHER.match(uri)){
+            case CODE_COIN_DIR:
+                final Context context = getContext();
+                if(context == null){
+                    return null;
+                }
+                final long id = AppDatabase.getInstance(context).coinDao().insert(Coin.fromContentValues(values));
+                return ContentUris.withAppendedId(uri,id);
+            case CODE_COIN_ITEM:
+                throw new IllegalArgumentException("Invalid URI, cannot insert with ID; " + uri);
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
     }
 
     @Override
